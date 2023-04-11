00dd5d558465933a39fc82c526a5dfefe0bba396
sumeetmore1997 <101966263+sumeetmore1997@users.noreply.github.com>
2023-04-11 14:11:44 +0530

    filter

--- routes/Post.route.js
+++ routes/Post.route.js
@@ -7,11 +7,32 @@ postRouter.get("/retrievedata",async(req,res)=>{
    // res.send("All the posts")
     let query=req.query
     try{
-        const posts=await PostModel.find(query)
-        res.send(posts)
+        // const posts=await PostModel.find(query)
+        // res.send(posts)
+        //const {query}=req
+        console.log(query)
+        let order=query._order
+        if(order=="asc")
+        {
+           const posts= await PostModel.find( query).sort({ budget: 1 })
+           //console.log(posts)
+           res.send({msg:"posts getting successfully ",posts})
+        }
+        else if(order="desc")
+        {
+            const posts= await PostModel.find( query).sort({ budget: -1 })
+            res.send({msg:"posts getting successfully ",posts})
+        }
+        else{
+            const posts= await PostModel.find( query)
+            res.send({msg:"posts getting successfully ",posts})
+        }
+        // const posts=await  order=="asc"? PostModel.find( query).sort({ budget: 1 }): order=="desc"? PostModel.find( query).sort({ budget: -1 }):PostModel.find( query)
+       
+        // res.send({msg:"posts getting successfully ",posts})
     }catch(err){
-        console.log(err)
-        res.send({"err":"Something went wrong"})
+        console.log(err.message)
+        res.send({"err":"Something went wrong",message:err.message})
     }
 })
 
